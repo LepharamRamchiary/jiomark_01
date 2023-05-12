d7fafe95af69a0e78f547a0f64fb9f0de969bebf
Lepha <lepharamchiary@gmail.com>
2023-05-12 23:45:47 +0530

    add some images and modify the some liens

--- src/Home/Body/Slider.js
+++ src/Home/Body/Slider.js
@@ -1,42 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 
 
 const Slider = () => {
+  const images = [
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683875837_Grand_Savings_on_Groceries.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683791687_Best_Selling_Smartphones.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683791687_Best_Selling_Smartphones.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683791930_Gone_in_a_Flash.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683876497_Unbeatable_offers_on_Kitchenware.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683876417_Affordable_Summer_Makeovers.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683875837_Grand_Savings_on_Groceries.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683809878_Never_Before_Deals_Air_Conditioners.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683875837_Grand_Savings_on_Groceries.jpg',
+    'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683875873_Mega_Offers_On_Top_Fashion_Styles.jpg'
+    // Add more image URLs here
+  ];
 
-    const images = [
-        'https://www.jiomart.com/images/cms/aw_rbslider/slides/1683130376_5G_Smartphones_v1.jpg',
-        // '../assets/Desktop2.jpg',
-        // Add more image URLs here
-    ];
+  const [currentImage, setCurrentImage] = useState(0);
 
-    const [currentImage, setCurrentImage] = useState(0);
+  const previousImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === 0 ? images.length - 1 : prevImage - 1
+    );
+  };
 
-    const previousImage = () => {
-        setCurrentImage((prevImage) => (prevImage === 0 ? images.length - 1 : prevImage - 1));
-    };
+  const nextImage = () => {
+    setCurrentImage((prevImage) =>
+      prevImage === images.length - 1 ? 0 : prevImage + 1
+    );
+  };
 
-    const nextImage = () => {
-        setCurrentImage((prevImage) => (prevImage === images.length - 1 ? 0 : prevImage + 1));
-    };
+  useEffect(() => {
+    const interval = setInterval(nextImage, 4000); // Set the interval duration in milliseconds (e.g., 3000 for 3 seconds)
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <button
-                className="absolute left-0 p-2 bg-gray-500 text-white"
-                onClick={previousImage}
-            >
-                Previous
-            </button>
-            <img src={images[currentImage]} alt="Slider" className="w-64" />
-            <button
-                className="absolute right-0 p-2 bg-gray-500 text-white"
-                onClick={nextImage}
-            >
-                Next
-            </button>
-        </div>
-
-    )
-}
+  return (
+    <div className="flex items-center justify-center mt-10 w-full">
+      <button
+        className="absolute left-3 p-2 bg-slate-300 text-gray-500 h-10 w-10 rounded-full"
+        onClick={previousImage}
+      >
+        {'<'}
+      </button>
+      <div className="flex-1 mx-4">
+        <div
+          className="w-screen h-80 bg-center bg-no-repeat bg-cover"
+          style={{ backgroundImage: `url(${images[currentImage]})` }}
+        ></div>
+      </div>
+      <button
+        className="absolute right-3 p-2 bg-slate-300 text-gray-500 h-10 w-10 rounded-full"
+        onClick={nextImage}
+      >
+        {'>'}
+      </button>
+    </div>
+  );
+};
 
 export default Slider;
