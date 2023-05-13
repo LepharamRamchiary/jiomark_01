4f5c351936fd88844f52e9ff6d9052b6d1a790b3
Lepha <lepharamchiary@gmail.com>
2023-05-13 21:54:18 +0530

    new page for posterHover

---
+++ src/Home/Body/PosterHover.js
@@ -0,0 +1,73 @@
+// import React from 'react'
+
+// function PosterHover() {
+//     return (
+//         <div className='w-full h-full flex'>
+//             <div
+//                 className="w-full h-20 m-5 bg-cover bg-center rounded-md relative"
+//                 style={{
+//                     backgroundImage: 'url("https://www.jiomart.com/images/cms/aw_rbslider/slides/1683833551_1240x209.jpg")',
+//                     backgroundSize: '100% 100%', // Adjusted property for full coverage
+//                     backgroundPosition: 'center',
+//                     width: '40%'
+//                 }}
+//             >
+//                 <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity duration-300"></div>
+//                 <div className="absolute inset-0 border-2 border-white opacity-0 hover:opacity-100 transition-opacity duration-300"></div>
+//                 {/* Content for the first rectangular container */}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default PosterHover;
+
+import React from 'react';
+
+function PosterHover() {
+    return (
+        <div className="w-full h-full flex cursor-pointer">
+            <div
+                className="w-full h-20 m-5 bg-cover bg-center rounded-md relative overflow-hidden"
+                style={{
+                    backgroundImage: 'url("https://www.jiomart.com/images/cms/aw_rbslider/slides/1683833551_1240x209.jpg")',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    width: '35%',
+
+                }}
+            >
+                <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity duration-300"></div>
+                <div className="absolute inset-0 border-2 border-white opacity-0 hover:opacity-100 transition-opacity duration-300"></div>
+                <img
+                    className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition-transform duration-300"
+                    src="https://www.jiomart.com/images/cms/aw_rbslider/slides/1683833551_1240x209.jpg"
+                    alt="Poster"
+                />
+                {/* Content for the rectangular container */}
+            </div>
+            <div
+                className="w-full h-20 mt-5 bg-cover bg-center rounded-md relative overflow-hidden"
+                style={{
+                    backgroundImage: 'url("https://www.jiomart.com/images/cms/aw_rbslider/slides/1683833613_1240x209.jpg")',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    width: '35%',
+                    
+                }}
+            >
+                <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity duration-300"></div>
+                <div className="absolute inset-0 border-2 border-white opacity-0 hover:opacity-100 transition-opacity duration-300"></div>
+                <img
+                    className="absolute inset-0 w-full h-full object-cover transform hover:scale-105 transition-transform duration-300"
+                    src="https://www.jiomart.com/images/cms/aw_rbslider/slides/1683833613_1240x209.jpg"
+                    alt="Poster"
+                />
+                {/* Content for the rectangular container */}
+            </div>
+        </div>
+    );
+}
+
+export default PosterHover;
+
