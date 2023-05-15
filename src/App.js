55a43ab8f7e09338304d87cda5f10811cfed8d32
Lepha <lepharamchiary@gmail.com>
2023-05-15 14:24:32 +0530

    import Sliderpath

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./Home/Navbar/Navbar";
 import Slider from "./Home/Body/Slider";
 import Poster from "./Home/Body/Poster";
 import PosterHover from "./Home/Body/PosterHover";
+import SliderPath from "./Home/Body/SliderPath";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Slider/>
       <Poster />
       <PosterHover />
+      <SliderPath />
     </div>
   );
 }
