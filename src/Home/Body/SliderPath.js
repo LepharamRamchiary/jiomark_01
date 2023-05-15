b8906c3ce7dfef913645a313901f485104403c45
Lepha <lepharamchiary@gmail.com>
2023-05-15 14:23:46 +0530

    create new page

---
+++ src/Home/Body/SliderPath.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SliderPath() {
+  return (
+    <div>
+      <h1 className='ml-5 text-lg font-bold'>Top Deals On Electronics</h1>
+    </div>
+  )
+}
+
+export default SliderPath;
